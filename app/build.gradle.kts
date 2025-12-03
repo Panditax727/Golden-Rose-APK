7ea2b9c0df99d90aae30105a6649919190f13e61
Panditax727 <juanpotito754@gmail.com>
2025-12-03 05:07:31 -0300

    fix: Register UI/UX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -63,4 +64,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.5")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
     implementation("androidx.activity:activity-compose:1.9.2")
+
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
 }
